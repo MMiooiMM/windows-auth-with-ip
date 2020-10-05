c5db41ad25638e4a964c63e53d0ca5e8b31b4d01
黃郁倫 mio <mio@miniasp.com>
2020-10-05 16:37:16 +0800

    以 JwtSecurityTokenHandler 來處理驗證

---
+++ windowsauth.ap/Handlers/CustomJwtSecurityTokenHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace windowsauth.ap.Handlers
+{
+    public class CustomJwtSecurityTokenHandler : ISecurityTokenValidator
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+        public CustomJwtSecurityTokenHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public bool CanValidateToken => true;
+
+        public int MaximumTokenSizeInBytes { get; set; } = TokenValidationParameters.DefaultMaximumTokenSizeInBytes;
+
+        public bool CanReadToken(string securityToken)
+        {
+            return _jwtSecurityTokenHandler.CanReadToken(securityToken);
+        }
+
+        public ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            var token = _jwtSecurityTokenHandler.ReadToken(securityToken) as JwtSecurityToken;
+            validatedToken = token;
+            var ip = token.Claims.FirstOrDefault(claim => claim.Type == "ip");
+
+            if (ip is null)
+            {
+                throw new SecurityTokenException();
+            }
+
+            var httpContextAccessor = _serviceProvider.GetService<IHttpContextAccessor>();
+
+            if (ip.Value != httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString())
+            {
+                throw new SecurityTokenValidationException();
+            }
+
+            return _jwtSecurityTokenHandler.ValidateToken(securityToken, validationParameters, out validatedToken);
+        }
+    }
+}
--- windowsauth.ap/Startup.cs
+++ windowsauth.ap/Startup.cs
@@ -1,20 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using windowsauth.ap.Middlewares;
+using windowsauth.ap.Handlers;
 
 namespace windowsauth.ap
 {
@@ -30,24 +22,21 @@ namespace windowsauth.ap
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(o =>
                 {
                     o.TokenValidationParameters = new TokenValidationParameters
                     {
                         NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
-
                         ValidateIssuer = true,
                         ValidIssuer = "issuer",
-
                         ValidateAudience = false,
-
                         ValidateLifetime = true,
-
                         ValidateIssuerSigningKey = false,
-
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456"))
                     };
+                    o.SecurityTokenValidators.Add(new CustomJwtSecurityTokenHandler(services.BuildServiceProvider()));
                 });
             services.AddControllers();
         }
@@ -63,8 +52,6 @@ namespace windowsauth.ap
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
-
-            app.UseMiddleware<JwtValidationMiddleware>();
 
             app.UseRouting();
 
